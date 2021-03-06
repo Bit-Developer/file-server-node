abb52b0eb5d5c369731848d9d7b79c8a8d1b035b
Rong Zhuang <jojozhuang@gmail.com>
2021-03-06 13:57:30 -0800

    add env files

---
+++ src/config/env.config.ts
@@ -0,0 +1,10 @@
+
+const config = {
+  port: 9000,
+  rootDir: '/Users/johnny/Downloads/',
+  webDir: '/Users/johnny/file-server-node/www',
+  request_delay: 0,
+  edit_mode: true
+};
+
+export default config;
